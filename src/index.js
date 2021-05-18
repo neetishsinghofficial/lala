179e9c94c5860302170dbf04661a04317fb25022
Neetish Singh <neetish.singh97@gmail.com>
2021-05-18 05:55:28 +0530

    ydb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 //var fs = require("fs");
+
+const request = require('request')
+const crypto = require('crypto')
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
@@ -81,7 +84,77 @@ fs.readFile('credentials.json', (err, content) => {
         }
     });
 }
+   function buystock(){
+    var baseurl = "https://api.coindcx.com"
 
+    var timeStamp = Math.floor(Date.now());
+    
+    // Place your API key and secret below. You can generate it from the website.
+    key = "02126160cdaa3723ce01be347011410ac8b9e86b9a09c93a";
+    secret = "88039d460a92873064e91ac5f5f136cb6494a0f44fbae5f2a6c688cfde7cde28";
+    
+    
+        body = {
+            "side": "buy",  //Toggle between 'buy' or 'sell'.
+            "order_type": "market_order", //Toggle between a 'market_order' or 'limit_order'.
+            "market": "TRXINR", //Replace 'SNTBTC' with your desired market pair. //This parameter is only required for a 'limit_order'
+            "total_quantity": 11, //Replace this with the quantity you want
+            "timestamp": timeStamp
+        }
+    
+        const payload = new Buffer(JSON.stringify(body)).toString();
+        const signature = crypto.createHmac('sha256', secret).update(payload).digest('hex')
+    
+        const options = {
+            url: baseurl + "/exchange/v1/orders/create",
+            headers: {
+                'X-AUTH-APIKEY': key,
+                'X-AUTH-SIGNATURE': signature
+            },
+            json: true,
+            body: body
+        }
+    
+        request.post(options, function(error, response, body) {
+            console.log(body);
+        })
+   }
+
+   function sellstock(){
+    var baseurl = "https://api.coindcx.com"
+
+    var timeStamp = Math.floor(Date.now());
+    
+    // Place your API key and secret below. You can generate it from the website.
+    key = "02126160cdaa3723ce01be347011410ac8b9e86b9a09c93a";
+    secret = "88039d460a92873064e91ac5f5f136cb6494a0f44fbae5f2a6c688cfde7cde28";
+    
+    
+        body = {
+            "side": "sell",  //Toggle between 'buy' or 'sell'.
+            "order_type": "market_order", //Toggle between a 'market_order' or 'limit_order'.
+            "market": "TRXINR", //Replace 'SNTBTC' with your desired market pair. //This parameter is only required for a 'limit_order'
+            "total_quantity": 11, //Replace this with the quantity you want
+            "timestamp": timeStamp
+        }
+    
+        const payload = new Buffer(JSON.stringify(body)).toString();
+        const signature = crypto.createHmac('sha256', secret).update(payload).digest('hex')
+    
+        const options = {
+            url: baseurl + "/exchange/v1/orders/create",
+            headers: {
+                'X-AUTH-APIKEY': key,
+                'X-AUTH-SIGNATURE': signature
+            },
+            json: true,
+            body: body
+        }
+    
+        request.post(options, function(error, response, body) {
+            console.log(body);
+        })
+   }
   function listMajors(auth,tt) {
     const sheets = google.sheets({ version: 'v4', auth });
     sheets.spreadsheets.values.get({
@@ -110,6 +183,12 @@ app.use(bodyParser.json());
 app.post('/buyorder', function (req, res) {
     valuea=JSON.stringify(req.body.price);
     type=JSON.stringify(req.body.type);
+
+    if(type=="buy"){
+      buystock();
+    }else if(type=="sell"){
+      sellstock();
+    }
     exchange=JSON.stringify(req.body.exchange);
     namew=JSON.stringify(req.body.name);
     volume=JSON.stringify(req.body.volume);
